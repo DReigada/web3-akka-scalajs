a3b03ac1fbd52b82a669993ea1bb6c378ed6d5e7
Daniel Reigada <daniel@codacy.com>
2017-12-30 00:40:58 +0000

    Working tweeth button

--- client/src/main/scala/com/dreigada/tweether/TweetherJs.scala
+++ client/src/main/scala/com/dreigada/tweether/TweetherJs.scala
@@ -1,30 +1,11 @@
 package com.dreigada.tweether
 
-import com.dreigada.contracts.Tweether
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import scala.scalajs.js
-
 
 object TweetherJs {
 
   def main(args: Array[String]): Unit = {
-    val tweether = Tweether("0x7de537ce72eceb976b7c2ffbd1c3ddb3c8807b20")
-    js.Dynamic.global.tw = tweether
-
-    showTweets(tweether)(10)
-
+    UI.initialize()
   }
 
-  def showTweets(tweether: Tweether)(count: Int): Future[Unit] = {
-    tweether.getNumberOfTweeths.map { ntweets =>
-      for {
-        i <- ntweets.value - Math.min(count, ntweets.value) to ntweets.value
-      } {
-        tweether.getTweeth(i).foreach(a => UI.showTweets(Seq(a)))
-      }
-    }
-  }
 
 }
--- client/src/main/scala/com/dreigada/tweether/UI.scala
+++ client/src/main/scala/com/dreigada/tweether/UI.scala
@@ -1,11 +1,42 @@
 package com.dreigada.tweether
 
-import com.dreigada.contracts.Tweeth
-import org.scalajs.jquery.{JQuery, jQuery}
+import com.dreigada.contracts.{Tweeth, Tweether}
+import org.scalajs.jquery.{JQuery, JQueryEventObject, jQuery}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.scalajs.js
 
 object UI {
 
-  def showTweets(tweeths: Seq[Tweeth]): Unit = {
+  def initialize(): Unit = {
+    val tweether = Tweether("0x7de537ce72eceb976b7c2ffbd1c3ddb3c8807b20")
+    js.Dynamic.global.tw = tweether
+
+    onClick("#tweeth-input-button") { _ =>
+      val text = jQuery("#tweeth-input").`val`().asInstanceOf[String]
+      println(s"Tweething $text")
+      tweether.makeTweeth(text)
+    }
+
+    getAndShowTweets(tweether)(10)
+  }
+
+  private def onClick(query: String)(body: JQueryEventObject => Unit): Unit = {
+    jQuery(query).on("click", body)
+  }
+
+  private def getAndShowTweets(tweether: Tweether)(count: Int): Future[Unit] = {
+    tweether.getNumberOfTweeths.map { ntweets =>
+      for {
+        i <- ntweets.value - Math.min(count, ntweets.value) to ntweets.value
+      } {
+        tweether.getTweeth(i).foreach(a => UI.showTweets(Seq(a)))
+      }
+    }
+  }
+
+  private def showTweets(tweeths: Seq[Tweeth]): Unit = {
     for {
       tweeth <- tweeths
       html = genTweeth(tweeth)

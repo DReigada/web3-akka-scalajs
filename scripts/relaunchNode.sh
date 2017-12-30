c5f9820952417d6fe87832bb77f5aea5061b010b
Daniel Reigada <daniel@codacy.com>
2017-12-30 22:15:09 +0000

    Add ganacheDB directory to gitignore

--- scripts/relaunchNode.sh
+++ scripts/relaunchNode.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run -d -p 8545:8545 -v $PWD/ganacheDB:/db dreigada/ganache-cli:latest -a 1 -u 0 --db /db
+docker run --name ganache-node -d -p 8545:8545 -v $PWD/ganacheDB:/db dreigada/ganache-cli:latest -a 1 -u 0 --db /db
 
 
--- scripts/startDev.sh
+++ scripts/startDev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p ganacheDB
-ganacheHash=$(docker run -d -p 8545:8545 -v $PWD/ganacheDB:/db dreigada/ganache-cli:latest -a 2 -u 0 -u 1 --db /db)
+ganacheHash=$(docker run --name ganache-node -d -p 8545:8545 -v $PWD/ganacheDB:/db dreigada/ganache-cli:latest -a 2 -u 0 -u 1 --db /db)
 
 docker run -d -p 8080:8080 dreigada/browser-solidity:latest
 
